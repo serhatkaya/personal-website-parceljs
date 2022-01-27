c4e6669efa1b1b750616cdb906eefccddba5b038
Serhat KAYA <serhat.kaya@emakina.com.tr>
2022-01-27 20:15:41 +0300

    about, experience sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import "./styles.css";
+'user strict';
+var jquery = require('jquery');
+window.$ = window.jQuery = jquery; // notice the definition of global variables here
+require('jquery-ui-dist/jquery-ui.js');
+var navMenuItems = $('#nav-menu li a');
+var sections = ['about', 'resume', 'projects', 'contact', 'blog'];
+
+$(function () {
+    navMenuItems.each((i, nav) => {
+        $(nav).on('click', function (event) {
+            event.preventDefault();
+        });
+    });
+});
+const animateCSS = (element, animation, prefix = 'animate__') =>
+    // We create a Promise and return it
+    new Promise((resolve, reject) => {
+        const animationName = `${prefix}${animation}`;
+        const node = document.querySelector(element);
+
+        node.classList.add(`${prefix}animated`, animationName);
+
+        // When the animation ends, we clean the classes and resolve the Promise
+        function handleAnimationEnd(event) {
+            event.stopPropagation();
+            node.classList.remove(`${prefix}animated`, animationName);
+            resolve('Animation ended');
+        }
+
+        node.addEventListener('animationend', handleAnimationEnd, {
+            once: true
+        });
+    });
